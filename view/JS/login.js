f3d27110434ad16e6e64ff01601c247a6c9c102d
HenriqueVieiraOliveira <henrique.vieira.tech@gmail.com>
2026-03-15 15:19:28 -0300

    Adição do index e remoção do style.css

--- view/JS/login.js
+++ view/JS/login.js
@@ -2,7 +2,7 @@ const togglePassword = document.getElementById('togglePassword')
 const password = document.getElementById('password')
 
 togglePassword.addEventListener('click', function(){
-    const type = password.getAttribute('type') === 'password' ? 'text' :' password'
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password'
     password.setAttribute('type', type)
 
     this.classList.toggle('fa-eye');
