ea6e84797b21347ec0e5a045f90a4c755eae378e
ThiagoMoura963 <thiagomjfelis3@gmail.com>
2026-03-27 13:55:05 -0300

    connection test

--- backend/infra/database.py
+++ backend/infra/database.py
@@ -35,9 +35,10 @@ class PostgresManager:
                 else:
                     self.conn.commit()
         finally:
-            if self.cursor:
-                self.cursor.close()
-            if self.conn:
-                self.conn.close()
+            ...
+            # if self.cursor:
+            #     self.cursor.close()
+            # if self.conn:
+            #     self.conn.close()
 
         return False
