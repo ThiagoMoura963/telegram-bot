b78f5e7c139113da04ba3df6ee5a47b1481a5ddd
HenriqueVieiraOliveira <henrique.vieira.tech@gmail.com>
2026-04-12 20:59:25 -0300

    Feature: JWT security configuration and route dependencies

---
+++ backend/core/deps.py
@@ -0,0 +1,30 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from backend.core.security import ALGORITHM
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+
+async def get_current_user_id(token: str = Depends(oauth2_scheme)) -> int:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Não foi possível validar as credenciais",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    try:
+        payload = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=[ALGORITHM])
+        
+        user_id: str = payload.get("sub")
+        
+        if user_id is None:
+            raise credentials_exception
+            
+        return int(user_id)
+        
+    except (JWTError, ValueError):
+        raise credentials_exception
---
+++ backend/core/security.py
@@ -0,0 +1,32 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class Security:
+    @staticmethod
+    def verify_password(plain_password: str, hashed_password: str) -> bool:
+        return pwd_context.verify(plain_password, hashed_password)
+
+    @staticmethod
+    def get_password_hash(password: str) -> str:
+        return pwd_context.hash(password)
+    
+    @staticmethod
+    def create_access_token(data: dict):
+        to_encode = data.copy()
+        now = datetime.now(timezone.utc)
+        expire = now + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+        to_encode.update({"exp": expire})
+
+        encoded_jwt = jwt.encode(to_encode, os.getenv('SECRET_KEY'), algorithm=ALGORITHM)
+        return encoded_jwt
