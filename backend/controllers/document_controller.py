ecd8a49400040de55cf16d7eee08af727eee5194
erickriato <erickriatopinheiro@hotmail.com>
2026-04-25 16:42:09 -0300

    feat: add get_document route and get_all method in document repository

--- backend/controllers/document_controller.py
+++ backend/controllers/document_controller.py
@@ -1,13 +1,23 @@
 from typing import Annotated
 
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, Depends, File, UploadFile
 
+from backend.core.deps import get_current_user_id
 from backend.infra.repositories.chunks_repository import ChunksRepository
 from backend.infra.repositories.document_resository import DocumentRepository
 from backend.processors.factory import get_document_processor
 from backend.services.document_service import DocumentService
 
 router = APIRouter(prefix='/api/v1/document', tags=['Document Manager'])
+
+
+@router.post('/{agent_id}')
+async def get_document(user_id: Annotated[str, Depends(get_current_user_id)], agent_id):
+
+    document_repository = DocumentRepository()
+    documents = document_repository.get_all(user_id, agent_id)
+
+    return {'documents': documents}
 
 
 @router.post('/upload')
